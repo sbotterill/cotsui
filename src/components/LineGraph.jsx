85a72c7e0da85067c4bcc1448eb89809a78979f7
Sonny Botterill <sonny.botterill@outlook.com>
2025-06-13 14:08:07 -0400

    updated line chart

--- src/components/LineGraph.jsx
+++ src/components/LineGraph.jsx
@@ -6,45 +6,36 @@ import { LicenseInfo } from '@mui/x-license';
 LicenseInfo.setLicenseKey("f66e40461091cc90836adea4ece3cdfaTz0xMTQ2NDAsRT0xNzgxNDgxNTk5MDAwLFM9cHJvLExNPXN1YnNjcmlwdGlvbixQVj1RMy0yMDI0LEtWPTI=");
 
 export default function LineChartWithReferenceLines(props) {
+  const total = props.chartDates.length;
+  const initialPoints = 100;
+  const endPct = Math.min(100, (initialPoints / total) * 100);
+
   return (
     <Box sx={{ width: '100%', height: '47vh' }}>
       <LineChartPro
         sx={{ width: '100%', height: '100%' }}
         series={[
-            { data: props.commericalChartData, label: 'Commericals', type: 'line' },
-            { data: props.nonCommercialChartData, label: 'Non-Commericals', type: 'line' },
-            { data: props.nonReportableChartData, label: 'Non-Reportables', type: 'line' },
+          { data: props.commericalChartData, label: 'Commercials', type: 'line' },
+          { data: props.nonCommercialChartData, label: 'Non-Commercials', type: 'line' },
+          { data: props.nonReportableChartData, label: 'Non-Reportables', type: 'line' },
         ]}
-        xAxis={[{ 
-            scaleType: 'point',
-            data: props.chartDates,
-            zoom: true,          // ← turn on zoom
-            min: 0,
-            max: 20, // Show last 20 data points initially
+        // ← full zoom object here:
+        xAxis={[{
+          data: props.chartDates,
+          scaleType: 'point',
+          zoom: {
+            minStart: 0,       // start at 0%
+            maxEnd:   endPct,  // show ~50 points
+            panning:  true,    // ← enable drag panning
+          },
         }]}
-        yAxis={[{ zoom: true }]}
+        yAxis={[{ zoom: true }]}      // y-axis can zoom/pan too if you like
         margin={{ right: 24, top: 20, bottom: 20, left: 50 }}
-        slotProps={{
-            legend: {
-            direction: 'row',
-            position: { vertical: 'top', horizontal: 'right' },
-            padding: 0,
-            },
-        }}
-        grid={{ horizontal: true }}
         tooltip={{ trigger: 'axis' }}
-        >
-        {/* draggable slider below the axis */}
-        <ChartZoomSlider 
-          sx={{
-            '& .MuiChartsAxis-tickLabel': {
-              fontSize: '0.75rem',
-              transform: 'rotate(-45deg)',
-              textAnchor: 'end',
-            },
-          }}
-        />
-        </LineChartPro>
+        slotProps={{
+          legend: { direction: 'row', position: { vertical: 'top', horizontal: 'right' } },
+        }}
+      />
     </Box>
   );
 }
